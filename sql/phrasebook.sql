a7303f8578c9822936f0bb8fec36110ed2c48bff
Rick Dearman <rick@rdearman.org>
2015-08-16 15:55:49 +0100

    added new tables.

--- sql/phrasebook.sql
+++ sql/phrasebook.sql
@@ -1,7 +1,7 @@
 ---
 --- This is an SQL file for use with a PostgreSQL DB system.
 ---
-DROP TABLE tblLanguageList;
+DROP TABLE tblLanguageList CASCADE;
 CREATE TABLE tblLanguageList (
        lang_id SERIAL PRIMARY KEY,
        lang_iso TEXT,
@@ -10,7 +10,7 @@ CREATE TABLE tblLanguageList (
        friendly_iso_locale_name TEXT
 );
 
-DROP TABLE tblphrase;
+DROP TABLE tblphrase  CASCADE;
 CREATE TABLE tblphrase (
        phrase_id SERIAL PRIMARY KEY,
        phrase_lang_id INTEGER 
@@ -18,10 +18,13 @@ CREATE TABLE tblphrase (
 		ON UPDATE CASCADE
 		ON DELETE CASCADE,
        phrase TEXT,
-       audio_location TEXT -- decided to go with file pointer, rather than binary storage.
+       audio_location TEXT, -- decided to go with file pointer, rather than binary storage.
+       up_votes INTEGER,
+       down_votes INTEGER,
+       active BOOLEAN DEFAULT TRUE
 );
 
-DROP TABLE tblphrasemapping;
+DROP TABLE tblphrasemapping  CASCADE;
 CREATE TABLE tblphrasemapping (
        lang_phrase_a INTEGER
        		REFERENCES tblphrase (phrase_id)
@@ -32,5 +35,48 @@ CREATE TABLE tblphrasemapping (
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
 );
-  
+
+---- of basic phrase functions ----
+
+---- Start of contributor / project section ----
+
+DROP TABLE tblcontributor  CASCADE;
+CREATE TABLE tblcontributor (
+       contributor_id SERIAL PRIMARY KEY,
+       handle TEXT,
+       contributor_password TEXT,
+       native_lang INTEGER -- only going to allow one native langage to work with, if you have more than one, sorry.
+       		REFERENCES tblLanguageList (lang_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
+);
+
+
+DROP TABLE tblproject  CASCADE;
+--- Create a project table who's primary key is a composite key of the two languages
+--- which are going to be put together. This includes the project managers contributor id.
+CREATE TABLE tblproject (
+       project_manager_id INTEGER
+       		REFERENCES tblcontributor (contributor_id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+       project_lang_a INTEGER 
+       		REFERENCES tblLanguageList (lang_id),
+       project_lang_b INTEGER 
+       		REFERENCES tblLanguageList (lang_id),
+       project_name TEXT,
+       PRIMARY KEY (project_lang_a, project_lang_b)
+);
+
+DROP TABLE tblcontributions  CASCADE;
+CREATE TABLE tblcontributions (
+       contributor_id INTEGER 
+                REFERENCES tblcontributor (contributor_id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE,
+       project_lang_a INTEGER,
+       project_lang_b INTEGER,
+       FOREIGN KEY (project_lang_a, project_lang_b) REFERENCES tblproject (project_lang_a, project_lang_b)
+
+);
 
